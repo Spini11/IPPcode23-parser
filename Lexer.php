4853694e99759ac79496f9173393ad642ceae239
Viliam Chudáčik <assassinspini@hotmail.com>
2023-02-14 12:43:54 +0100

    Created regex for commands

--- Lexer.php
+++ Lexer.php
@@ -1,16 +1,33 @@
 <?php
 
-class LexToken
+class Lexems
 {
-    public $type;
-    public $name;
 
-    function GetLexeme($input)
+    public static function IsCommentWhite($input)
     {
+        if(preg_match('/^\s*$/',$input) || preg_match('/^\s*#/', $input))
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    private static function GetLexeme($input)
+    {
+        if(preg_match('/^\s*((CREATEFRAME)|(PUSHFRAME)|(POPFRAME)|(RETURN)|(BREAK))\s*$/',$input)) return 1;
+        if(preg_match('/^\s*(DEFWAR|CALL|PUSHS|POPS|WRITE|LABEL|JUMP|EXIT|DPRINT)\s*$/', $input)) return 2;
+        if(preg_match('/^\s*(MOVE|INT2CHAR|READ|TYPE)\s*$/', $input)) return 3;
+        if(preg_match('/^\s*(ADD|SUB|MUL|IDIV|LT|GT|EQ|AND|OR|NOT|STRI2INT|CONCAT|GETCHAR|SETCHAR|JUMPIFEQ)\s*$/')) return 4;
 
     }
-    function IsComment($input)
+
+    public static function Tokenize($input, &$tokens)
     {
-        return preg_match('/^#/',$input);
+        if(self::IsCommentWhite($input))
+        {
+            return;
+        }
+
+        self::GetLexeme($input);
     }
 }
--- parse.php
+++ parse.php
@@ -3,15 +3,13 @@ require_once('Lexer.php');
 $f = fopen( 'php://stdin', 'r' );
 while(($line = fgets($f)) != ".IPPcode23\n")
 {
-    if(!preg_match('/^\s*$/',$line) && !preg_match('/^\s*#/',$line))
-        {
-            exit(21);
-        }
+    if(!Lexems::IsCommentWhite($line))
+        exit(21);
 }
+$tokens = [];
 while( $line = fgets( $f ) ) {
-    $delimiter = ' ';
-    $words = explode($delimiter, $line);
-
+    Lexems::Tokenize($line, $tokens);
+    print_r($tokens);
 }
 
 fclose( $f );
